4219c87eddcbd9e58543fd51275ec46eca859e04
Ankit Rawat <55525947+ankit215@users.noreply.github.com>
2019-09-24 16:29:21 +0530

    Update asses3.js

--- asses3.js
+++ asses3.js
@@ -1,12 +1,7 @@
 //QUESTION 3.
-var introduction = ["Hello", "I" , "am", "Ankit"];
-var greeting = introduction[0];
-var name = introduction[3];
-
-console.log(greeting);//"Hello"
-console.log(name);//"Ankit"
-
 /*DESTRUCTIVE ASSIGN =>Destructuring assignment is a cool feature that came along with ES6. 
 Destructuring is a JavaScript expression that makes it possible to unpack values from arrays, 
 or properties from objects, into distinct variables. That is, we can extract data from arrays 
 and objects and assign them to variables.*/ 
+var [Hello, I , am, Ankit]=['greeting',2, 3, 'name'] ;
+console.log(Hello); // greeting
